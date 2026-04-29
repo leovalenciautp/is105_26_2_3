dbd2e9a94f326bcc1c7fbc08c5abb62c5aadc86e
Leonardo Valencia <leov@gammawizard.com>
2026-04-29 16:53:01 -0400

    Agregando roca

---
+++ clase_4_29/Rock.fs
@@ -0,0 +1,90 @@
+module App.Rock
+
+open System
+open System.Threading
+open App.Utils
+
+type RockState =
+| Falling
+| Terminated
+
+type State = {
+    RockState: RockState
+    X: int
+    Y: int
+    RedrawScreen: bool
+    Tick: int
+}
+
+let initialState = {
+    RockState = Falling
+    X = Console.BufferWidth/2
+    Y = 0
+    RedrawScreen = true
+    Tick = -1
+}
+
+let updateTick state =
+    {state with Tick = state.Tick+1}
+
+let updateRock state =
+    let newState =
+        if state.Tick <> 0 && state.Tick % 4 = 0 then
+            {state with Y= min (Console.BufferHeight-1) (state.Y+1)}
+        else
+            state 
+    if newState <> state then
+        {newState with RedrawScreen = true}
+    else
+        state
+
+
+let redrawScreen state =
+    if state.RedrawScreen then 
+        Console.Clear()
+        displayMessage state.X state.Y ConsoleColor.Red "@"
+        {state with RedrawScreen = false}
+    else
+        state
+
+let updateRockKeyboard key state =
+    let newState =
+        match key with 
+        | ConsoleKey.Enter ->
+            {state with Y=0}
+        | ConsoleKey.Escape ->
+            {state with RockState = Terminated}
+        | _ -> state
+
+    if newState <> state then 
+        {newState with RedrawScreen=true}
+    else
+        state
+let processKeyboard state =
+    if Console.KeyAvailable then 
+        let k = Console.ReadKey true
+        state
+        |> updateRockKeyboard k.Key
+    else
+        state
+let rec mainLoop state =
+    let newState = 
+        state
+        |> updateTick // Este es un timer
+        |> updateRock
+        |> processKeyboard
+        |> redrawScreen
+    if newState.RockState = Falling then 
+        Thread.Sleep 25
+        mainLoop newState
+
+let mostrar() =
+    let oldForeground = Console.ForegroundColor
+    Console.CursorVisible <- false
+
+    initialState
+    |> mainLoop
+
+    Console.CursorVisible <- true
+    Console.ForegroundColor <- oldForeground
+    Console.Clear()
