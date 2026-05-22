a88ebe32423c869566f6c5c7190e08b18f203004
Leonardo Valencia <leov@gammawizard.com>
2026-05-22 15:08:49 -0400

    Agregando misiles

--- mailbox/Program.fs
+++ mailbox/Program.fs
@@ -11,11 +11,17 @@
 open System
 
 
+type Misil = {
+    X: int
+    Y: int
+}
+
 type State = {
     Clock1: int
     Clock2: int
     AlienX: int
     AlienY: int
+    Misiles: Misil list
 }
 
 type Mensaje =
@@ -23,6 +29,7 @@ type Mensaje =
 | TempoDosHizoClick
 | RefreshScreen
 | TeclaPresionada of ConsoleKey
+| AnimarMisil
 
 let displayMessage x y color (msg:string) =
     Console.SetCursorPosition(x,y)
@@ -42,12 +49,19 @@ let displayClock2 state =
 
 let displayAlien state =
     displayMessage state.AlienX state.AlienY ConsoleColor.Yellow "👽"
+
+let displayMisiles state =
+    state.Misiles
+    |> List.iter (fun misil -> 
+        displayMessage misil.X misil.Y ConsoleColor.Yellow "=>"
+    )
 let refreshScreen state =
     Console.Clear()
     [|
         displayClock1
         displayClock2
         displayAlien
+        displayMisiles
     |]
     |> Array.iter (fun f -> state |> f)
     
@@ -58,6 +72,7 @@ let initialState = {
     Clock2 = 0
     AlienX = Console.BufferWidth/2
     AlienY = Console.BufferHeight/2
+    Misiles = []
 }
 
 let procesarTecla key state =
@@ -70,8 +85,25 @@ let procesarTecla key state =
             {state with AlienX = max 0 (state.AlienX-1)}
         | ConsoleKey.RightArrow ->
             {state with AlienX = min (Console.BufferWidth-2) (state.AlienX+1)}
+        | ConsoleKey.Spacebar ->
+            let nuevoMisil = {
+                X = state.AlienX+2
+                Y = state.AlienY
+            }
+            {state with Misiles = nuevoMisil :: state.Misiles}
         | _ -> state
 
+
+let actualizarMisiles state =
+    if state.Misiles <> [] then 
+        state.Misiles
+        |> Seq.map (fun misil -> {misil with X=misil.X+1})
+        |> Seq.filter (fun misil -> misil.X < Console.BufferWidth-2)
+        |> Seq.toList
+        |> fun nuevosMisiles ->
+            {state with Misiles = nuevosMisiles} 
+    else
+        state
 let updateState oldState message =
     match message with 
     | TempoUnoHizoClick ->
@@ -82,6 +114,8 @@ let updateState oldState message =
         oldState |> refreshScreen
     | TeclaPresionada k ->
         oldState |> procesarTecla k
+    | AnimarMisil ->
+        oldState |> actualizarMisiles
 
 //
 // Para enviar y recibir mensajes
@@ -119,6 +153,13 @@ let rec redibujar() =
         return! redibujar()
     }
 
+let rec animar() =
+    async {
+        do! Async.Sleep 25
+        buzon.Post AnimarMisil
+        return! animar()
+    }
+
 let rec leerTeclado() =
     async {
         let salir = 
@@ -137,6 +178,7 @@ Console.CursorVisible <- false
 tempoUno() |> Async.StartImmediate
 tempoDos() |> Async.StartImmediate
 redibujar() |> Async.StartImmediate
+animar() |> Async.StartImmediate
 
 leerTeclado() |> Async.RunSynchronously
 
